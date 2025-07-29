66f3e94746702d031942ebb329370806c6ba4399
javihidalgo78 <jhidlop267@g.educaand.es>
2025-07-29 13:54:10 +0200

    ya

--- update_schema.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `productos`
-ADD `tallas` TEXT NULL DEFAULT NULL AFTER `caracteristicas`,
-ADD `capacidades` TEXT NULL DEFAULT NULL AFTER `tallas`,
-ADD `colores` TEXT NULL DEFAULT NULL AFTER `capacidades`;
